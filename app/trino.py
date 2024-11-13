518e4d45b68dcaa6dbd2647eefefc80a44e15e45
ALEJANDRO CRUZ MONTIEL <alecru@deacero.com>
2024-11-13 12:47:44 -0600

    Añadir logica para tambien agregar joins a los queries de trino

--- app/trino.py
+++ app/trino.py
@@ -10,13 +10,37 @@ class SQLOperations:
         user_pass = f'{TRINO_USER}:{TRINO_PASS}' if TRINO_PASS else TRINO_USER
         self.engine = create_engine(f'trino://{user_pass}@{TRINO_HOST}:{TRINO_PORT}/{catalog}')
 
-    def execute_query(self, modelo: Base, *filters, page_size: int = 10, page_number: int = 1,
+    def execute_query(self, modelo: Base, *filters, joins=None, page_size: int = 10, page_number: int = 1,
                       order_by: str = None):
+        '''
+        :param modelo: Es el modelo de la base de datos a las que se hara el query
+        :param filters: Estos argumentos son los filtros se usan como positional arguments, ej:
+                        modelo_1.columna_1 == 5, modelo_1.columna_2 > 0
+        :param joins: Lista de tuplas con los joins la primera posicion de la tupla es el modelo,la segunda es la
+                      condicion y la tercera es si es 'left'(outerjoin) o 'inner'(join), ej:
+                      (modelo_2, modelo_1.columna_3 == modelo_2.columna_1, 'inner')
+        :param page_size: Numero de resultados en la pagina
+        :param page_number: Numero de pagina
+        :param order_by: Campo por el cual se va a ordenar los resultados
+        :return:
+        '''
+        joins = joins or []
         offset_value = (page_number - 1) * page_size if page_size and page_number else None
         session = self._create_session()
+        query = session.query(modelo)
+
+        for join_info in joins:
+            if isinstance(join_info, tuple):
+                table, condition, join_type = join_info
+                if join_type == 'left':
+                    query = query.outerjoin(table, condition)
+                else:
+                    query = query.join(table, condition)
+            else:
+                query = query.join(join_info)
+
         results = (
-            session.query(modelo)
-            .filter(*filters)
+            query.filter(*filters)
             .order_by(order_by)
             .offset(offset_value)
             .limit(page_size)
